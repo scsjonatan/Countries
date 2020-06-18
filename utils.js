03f0e8b9f73180237b754b3b4ada30aea26b84be
Jonatan Santa Cruz <jonatan.santacruz@schibsted.com>
2020-06-18 01:48:59 -0500

    move replace methods

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 export function replaceText(text) {
   return text
+    .toLowerCase()
     .replace('å', 'a')
     .replace('ç', 'c')
     .replace('ô', 'o')
     .replace('é', 'e')
-    .toLowerCase()
 }
